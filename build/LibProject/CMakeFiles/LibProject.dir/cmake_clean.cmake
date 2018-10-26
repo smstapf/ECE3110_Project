b3ade2c5cb5e8fc507a7743274b67a9bb9bd59df
Marshall <mblgh6@mail.missouri.edu>
2018-10-26 14:48:33 -0500

    Removed the build directory.. trying to get the gitignore to work.

--- build/LibProject/CMakeFiles/LibProject.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LibProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/LibProject/CMakeFiles/LibProject.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LibProject.
-# This may be replaced when dependencies are built.
